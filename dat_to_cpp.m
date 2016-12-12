7aee99a5122152a7f429654ba9d624f44d18792d
kacle <kacz.levente@gmail.com>
2016-12-12 11:28:33 +0100

    Clear code

--- dat_to_cpp.m
+++ dat_to_cpp.m
@@ -1,24 +1,22 @@
-poziciok =  'poziciok.dat';
-fotocsucsok =  'fotocsucs.dat';
 format long
+% initialize input
+poziciok =  'poziciok.dat';
 poziciok_xy = importdata(poziciok).*100000;
 poziciok_xy = poziciok_xy';
+tu_index = 1:400;
+poziciok_xyi = [ tu_index; poziciok_xy];
 
-fotocsucsok = 1:400;
-% fotocsucsok = fotocsucsok';
-
-poziciok_xyi = [ fotocsucsok; poziciok_xy];
-%fileID = fopen('poziciok.txt', 'w');
-%fprintf(fileID, '%d %d\n', fotocsucs2);
-
+%build kd_tree
 kd_matrix_values = kd_matrix(poziciok_xyi, 9);
 
-pozi = [37675806;47522527]; %99. elem a kd_matrix_value-ban
+% find random element of the poziciok.dat
+pozi = [37675806;47522527];
 tu_index = kd_search(kd_matrix_values, pozi);
 
+%plot the results
 hold on
 plot(kd_matrix_values(18, 1:400), kd_matrix_values(19, 1:400), 'r*')
-title('KD-tree')
 plot(pozi(1), pozi(2), 'b*')
 plot(tu_index(2), tu_index(3), 'k*')
+title('KD-tree')
 legend('Leafs', 'Input point', 'Output point')
